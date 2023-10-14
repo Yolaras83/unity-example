caf096fe0f7a18c10e08ce106757319d60ad9bbb
Nikolakis831656 <94140646+Yolaras83@users.noreply.github.com>
2023-10-14 10:54:48 +0300

    Added A Finish Line Feature
    -Fixed Camera Position
    
    - Added A Basic Finish Line
    
    - A Finish Animation
    
    - Made A Finish UI
    
    - Added Working Buttons

---
+++ Hyper Casual Testing/Assets/Scenes/FinishTrigger.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishTrigger : MonoBehaviour
+{
+    public GameObject CoinSpawner;
+    public Movement PMovement;
+    public Rigidbody rb;
+    public Animator anim;
+    public GameObject WinOverlay;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+    public void OnTriggerEnter(Collider collision)
+    {
+        if(collision.GetComponent<BoxCollider>().CompareTag("Finish"))
+            {
+            CoinSpawner.SetActive(false);
+            PMovement.enabled = false;
+            anim.SetBool("Run", false);
+            anim.SetBool("Win", true);
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        rb.AddForce(0f, 0f, 10f);
+    }
+
+    public void ShowUIAfterWinAnimation()
+    {
+        WinOverlay.SetActive(true);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+}
