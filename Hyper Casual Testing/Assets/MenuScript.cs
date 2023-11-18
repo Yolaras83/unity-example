32de3df5b0eda8de8d8823573ce1d31cd799f588
Nikolakis831656 <94140646+Yolaras83@users.noreply.github.com>
2023-11-18 11:26:13 +0200

    Sound Effects Menu

--- Hyper Casual Testing/Assets/MenuScript.cs
+++ Hyper Casual Testing/Assets/MenuScript.cs
@@ -5,22 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public AudioSource PlayAS;
+    public AudioSource SettingsAS;
+    public AudioSource QuitAS;
     public void Play()
     {
-        int randomSceneIndex = Random.Range(1, SceneManager.sceneCountInBuildSettings);
-        SceneManager.LoadScene(randomSceneIndex);
-        Debug.Log("Scene No. " + randomSceneIndex + " Loaded");
+        StartCoroutine(DelayPlay());
     }
 
    public void Settings()
     {
         //Do Something Little Nigga Dont Keep Staring At Me Remove Me And Start Coding Bitch
         Debug.Log("This Will Open Settings!");
+        SettingsAS.Play();
+
     }
 
     public void Quit()
     {
         Application.Quit();
         Debug.Log("This Will Close The Game");
+        QuitAS.Play();
+    }
+    
+    IEnumerator DelayPlay()
+    {
+        PlayAS.Play();
+        yield return new WaitForSeconds(1.4f);
+        int randomSceneIndex = Random.Range(1, SceneManager.sceneCountInBuildSettings);
+        SceneManager.LoadScene(randomSceneIndex);
+        Debug.Log("Scene No. " + randomSceneIndex + " Loaded");
     }
 }
